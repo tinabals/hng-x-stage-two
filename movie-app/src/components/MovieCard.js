eb21dd88cc537caab8e6a10fc153ae6b8d8f0512
Damilola Balogun <balogundamilola.f@gmail.com>
2023-09-15 11:04:00 +0100

    added the add to favorite implementation

--- movie-app/src/components/MovieCard.js
+++ movie-app/src/components/MovieCard.js
@@ -1,28 +1,50 @@
+import React, { useState } from 'react';
 import '../styles/MovieCard.css';
 import { Link } from 'react-router-dom';
+
 const MovieCard = ({ movie }) => {
-  
+  const [isLiked, setIsLiked] = useState(false);
+
+  const toggleLike = () => {
+    setIsLiked(!isLiked);
+  };
+
   return (
-    <div className='movie-card-container'>
-    <Link to={`/movies/${movie.id}`} >
-        <div className='movie-card'>
-          <div className='movie-poster-container'>
-            <img className='movie-image' src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`} alt={movie.title}  data-testid='movie-poster' />
+    <div className={`movie-card-container${isLiked ? ' liked' : ''}`}>
+    <div className='movie-card'>
+    <div className='movie-poster-container'>
+    <Link to={`/movies/${movie.id}`}>
+    <img
+              className='movie-image'
+              src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+              alt={movie.title}
+              data-testid='movie-poster'
+            />
+            </Link>
           </div>
           <div className='movie-details'>
-            <p className='movie-title' data-testid='movie-title'>{movie.title}</p>
-            <p className='movie-release-date' data-testid='movie-release-date'>{movie.release_date}</p>
-            <p className="rating">{movie.vote_average}</p>
-            <span className="genre">Action, Adventure, Horror</span>
+            <p className='movie-title' data-testid='movie-title'>
+              {movie.title}
+            </p>
+            <p className='movie-release-date' data-testid='movie-release-date'>
+              {movie.release_date}
+            </p>
+            <p className='rating' data-testid='movie-rating'>{movie.vote_average}</p>
+            <span className='genre'>Action, Adventure, Horror</span>
+          </div>
+          <div
+            className={`love-icon${isLiked ? ' liked' : ''}`}
+            onClick={toggleLike}
+          >
+            <i
+              className={`fa fa-heart`}
+              style={{ fontSize: '38px', color: isLiked ? 'black' : 'red' }}
+            ></i>
           </div>
         </div>
-        </Link>
      
     </div>
   );
 };
 
 export default MovieCard;
-
-
-
